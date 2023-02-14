b6cf544dc3d8b6af6cdea078e01581b06411fb44
lkuca <120181604+lkuca@users.noreply.github.com>
2023-02-15 00:26:36 +0200

    Update praktiline_palgad.py

--- praktiline_palgad/praktiline_palgad.py
+++ praktiline_palgad/praktiline_palgad.py
@@ -9,7 +9,7 @@ while True:
         inimesed,palgad=Lisa_andmed(inimesed,palgad)
         print("inimesed")
         print("palgad")
-        menu=int(input("Valik:\n1-Lisa andmed\n2-Kustuta andmed\n3-Suurim palk\n4 väikdem\n5 sorteeritud\n"))
+        menu=int(input("Valik:\n1-Lisa andmed\n2-Kustuta andmed\n3-Suurim palk\n4 väikdem\n5 sorteeritud\n6 vorsed palgad\n7 imja\n8 suurim vaike\n"))
         if menu==0:
             break
         elif menu==1:
@@ -24,18 +24,24 @@ while True:
             print(f"väiksem palk on{palk} {nimi}´l")
         elif menu==5:
             inimesed,palgad=sorteerimine(inimesed,palgad)
-            print(f"sorteeritud palk on {palk} {nimi}")
+            print(f"sorteeritud palk on {palgad} {inimesed}")
         elif menu==7:
-            inimesed,palgad=imja(inimesed,palgad)
-            print(f"imja palk on {palk} {nimi}")
+            imja(inimesed,palgad)
         elif menu==12:
             palgad, inimesed=sorteerimine(palgad,inimesed)
             print(inimesed,palgad)
         elif menu==6:
-             inimesed ,palgad=vordsed_palgad(inimesed,palgad)
-             print(f"võrdne palk on {palk} {nimi}")
+             vordsed_palgad(inimesed,palgad)
         elif menu==13:
             inimesed,palgad=kustuta(inimesed,palgad)
         elif menu==14:
             inimesed,palgad=redact(inimesed,palgad)
-            print(f"jengpkegrrg on {nimi} {palk}")
+            print(f"jengpkegrrg on {inimesed} {palgad}")
+        elif menu==8:
+            suur_vaike(inimesed,palgad)
+        elif menu==10:
+            keskminepalk(inimesed,palgad)
+        elif menu==9:
+            top3(inimesed,palgad)
+        elif menu==11:
+            tulumaks()
